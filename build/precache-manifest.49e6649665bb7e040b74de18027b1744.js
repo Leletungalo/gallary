210825f5a883edb45eca0f21c14ae4fb174b867d
leletu ngalo <lelethungalo@gmail.com>
2020-11-18 14:41:31 +0200

    added back to log in

---
+++ build/precache-manifest.49e6649665bb7e040b74de18027b1744.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cff5f84ae1cf4f4274e443f12ed0cf97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89116f3d61f6f8755eeb",
+    "url": "/static/css/main.251279b4.chunk.css"
+  },
+  {
+    "revision": "94f2c1c98b7a88079981",
+    "url": "/static/js/2.5533cb03.chunk.js"
+  },
+  {
+    "revision": "5ac922b1a6e253946f13fa455dc8d57c",
+    "url": "/static/js/2.5533cb03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89116f3d61f6f8755eeb",
+    "url": "/static/js/main.343b841c.chunk.js"
+  },
+  {
+    "revision": "506ecc995e3e932d4f6b",
+    "url": "/static/js/runtime-main.1b1a4d3d.js"
+  }
+]);
